afc6c9c5203cdcec71cc36588b3b0d0bfa073e71
JemilatOlatunji <oladaradala@gmail.com>
2026-07-20 18:34:46 +0100

    Added dark mode feature

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ console.log("My name is " + name);
 console.log("I study " + course);
 
 alert("Welcome to Jemilat's Portfolio!");
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
